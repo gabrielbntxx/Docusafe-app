9a7fb54fb25fd28ce0c1cd6d3d69ddfb585ca384
Gabriel Benetrix <gabrielbenetrix@MacBook-Pro-de-Gabriel-3.local>
2026-01-14 21:30:33 +0100

    Force bind to 0.0.0.0 instead of container hostname

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "0c6c88cc59859837"; }
+/******/ 		__webpack_require__.h = function() { return "4f24e56e067f77c0"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- server.js
+++ server.js
@@ -3,12 +3,12 @@ const { parse } = require('url')
 const next = require('next')
 
 const port = parseInt(process.env.PORT || '3000', 10)
-const hostname = process.env.HOSTNAME || '0.0.0.0'
+const hostname = '0.0.0.0' // Always bind to 0.0.0.0 for Railway
 const dev = process.env.NODE_ENV !== 'production'
 
 console.log(`Environment PORT: ${process.env.PORT}`)
 console.log(`Using port: ${port}`)
-console.log(`Using hostname: ${hostname}`)
+console.log(`Binding to: ${hostname}`)
 
 const app = next({ dev })
 const handle = app.getRequestHandler()
