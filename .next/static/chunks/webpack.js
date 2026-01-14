4e2800962171c57b964e733a59bf1e5b9e57b0af
Gabriel Benetrix <gabrielbenetrix@MacBook-Pro-de-Gabriel-3.local>
2026-01-14 21:27:37 +0100

    Add logging and bind to 0.0.0.0 for Railway

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "af4efffcc56dc763"; }
+/******/ 		__webpack_require__.h = function() { return "0c6c88cc59859837"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- server.js
+++ server.js
@@ -3,7 +3,13 @@ const { parse } = require('url')
 const next = require('next')
 
 const port = parseInt(process.env.PORT || '3000', 10)
+const hostname = process.env.HOSTNAME || '0.0.0.0'
 const dev = process.env.NODE_ENV !== 'production'
+
+console.log(`Environment PORT: ${process.env.PORT}`)
+console.log(`Using port: ${port}`)
+console.log(`Using hostname: ${hostname}`)
+
 const app = next({ dev })
 const handle = app.getRequestHandler()
 
@@ -11,8 +17,8 @@ app.prepare().then(() => {
   createServer((req, res) => {
     const parsedUrl = parse(req.url, true)
     handle(req, res, parsedUrl)
-  }).listen(port, (err) => {
+  }).listen(port, hostname, (err) => {
     if (err) throw err
-    console.log(`> Ready on http://localhost:${port}`)
+    console.log(`> Ready on http://${hostname}:${port}`)
   })
 })
